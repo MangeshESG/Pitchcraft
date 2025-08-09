22198d1553c7c86aca654386542cb2ca1f4b459f
atabrez <aabid.tabrez@hpe.com>
2025-08-09 16:38:59 +0530

    Changes for output sectioin as suggested by Rushikesh.

--- src/components/feature/Output.tsx
+++ src/components/feature/Output.tsx
@@ -1112,28 +1112,7 @@ const Output: React.FC<OutputInterface> = ({
     <div className="login-box gap-down">
       <div className="d-flex justify-between align-center mb-20 border-b pb-[15px] mb-[15px]">
 <div className="control-buttons d-flex align-center">
-  {isResetEnabled ? ( // Changed from !isProcessing to isResetEnabled
-    <button
-      className="primary-button bg-[#3f9f42]"
-      onClick={() => handleStart?.(currentIndex)}
-      disabled={
-        (!selectedPrompt?.name || !selectedZohoviewId) &&
-        !selectedCampaign
-      }
-      title={`Click to generate hyper-personalized emails starting from contact ${currentIndex + 1}`}
-    >
-      Generate
-    </button>
-  ) : (
-    <button
-      className="secondary-button"
-      onClick={handleStop}
-      disabled={isStopRequested} // Disable if stop is already requested
-      title="Click to stop the generation of emails"
-    >
-      Stop
-    </button>
-  )}
+  
   
   {userRole === "ADMIN" && (
     <button
@@ -1378,7 +1357,7 @@ const Output: React.FC<OutputInterface> = ({
                 <button
                   onClick={handlePrevPage}
                   disabled={isProcessing || currentIndex === 0}
-                  className="secondary-button flex justify-center items-center px-[10px] h-[35px]"
+                  className="secondary-button flex justify-center items-center !px-[10px] h-[35px]"
                   title="Click to go to the previous generated email"
                 >
                   <img
@@ -1398,7 +1377,7 @@ const Output: React.FC<OutputInterface> = ({
                 <button
                   onClick={handleNextPage}
                   disabled={isProcessing || currentIndex === combinedResponses.length - 1}
-                  className="secondary-button !h-[35px] !py-[10px] flex justify-center items-center"
+                  className="secondary-button !h-[35px] !py-[10px] !px-[10px] flex justify-center items-center"
                   title="Click to go to the next generated email"
                 >
                   <span>Next</span>
@@ -1445,7 +1424,26 @@ const Output: React.FC<OutputInterface> = ({
                 {combinedResponses.length > 0 && (
                   <>
                     <span>
-                      <strong>Contact:</strong> {currentIndex + 1} of{" "}
+                      {/* <strong>Contact:</strong> {currentIndex + 1} of{" "} */}
+                      <strong>Contact:</strong> 
+                      {/* Input box to enter index */}
+                      <input
+                        type="number"
+                        value={inputValue}
+                        onChange={handleIndexChange}
+                        onBlur={() => {
+                          // When input loses focus, ensure it shows a valid value
+                          if (
+                            inputValue.trim() === "" ||
+                            isNaN(parseInt(inputValue, 10))
+                          ) {
+                            setInputValue((currentIndex + 1).toString());
+                          }
+                        }}
+                        className="form-control text-center !mx-2"
+                        style={{ width: "70px", padding: "8px" }}
+                      />
+                       of{" "}
                       {
                         // Get total contacts from the selected view or all views
                         selectedZohoviewId
@@ -1470,28 +1468,37 @@ const Output: React.FC<OutputInterface> = ({
                     <span style={{ whiteSpace: "pre" }}> </span>
                     <span style={{ whiteSpace: "pre" }}> </span>
 
-                    {/* Input box to enter index */}
-                    <input
-                      type="number"
-                      value={inputValue}
-                      onChange={handleIndexChange}
-                      onBlur={() => {
-                        // When input loses focus, ensure it shows a valid value
-                        if (
-                          inputValue.trim() === "" ||
-                          isNaN(parseInt(inputValue, 10))
-                        ) {
-                          setInputValue((currentIndex + 1).toString());
-                        }
-                      }}
-                      className="form-control text-center mx-2"
-                      style={{ width: "70px", padding: "8px" }}
-                    />
+                    
                   </>
                 )}
               </div>
             </ul>
-            <div className="!mb-[0px] flex align-center">
+            <div className="flex">
+              <div className="flex mr-4">
+                {isResetEnabled ? ( // Changed from !isProcessing to isResetEnabled
+                  <button
+                    className="primary-button bg-[#3f9f42]"
+                    onClick={() => handleStart?.(currentIndex)}
+                    disabled={
+                      (!selectedPrompt?.name || !selectedZohoviewId) &&
+                      !selectedCampaign
+                    }
+                    title={`Click to generate hyper-personalized emails starting from contact ${currentIndex + 1}`}
+                  >
+                    Generate
+                  </button>
+                ) : (
+                  <button
+                    className="primary-button bg-[#3f9f42]"
+                    onClick={handleStop}
+                    disabled={isStopRequested} // Disable if stop is already requested
+                    title="Click to stop the generation of emails"
+                  >
+                    Stop
+                  </button>
+                )}
+              </div>
+              <div className="!mb-[0px] flex align-center">
                 <label className="checkbox-label !mb-[0px] mr-[5px] flex align-center">
                   <input
                     type="checkbox"
@@ -1514,6 +1521,8 @@ const Output: React.FC<OutputInterface> = ({
                   </svg>
                 </span>
               </div>
+              
+            </div>
             <div className="d-flex flex-col-1200 mb-10-991 mt-10-991">
               <div className="d-flex flex-col-768 mt-10-1200">
                 {/* Add the Excel export link */}
@@ -1706,11 +1715,11 @@ const Output: React.FC<OutputInterface> = ({
                     <span style={{ whiteSpace: "pre" }}> </span>
                     {combinedResponses[currentIndex]?.location || "NA"}
                     <span style={{ whiteSpace: "pre" }}> </span>
-                    <span className="inline-block relative top-[6px] mr-[3px]">
+                    {/* <span className="inline-block relative top-[6px] mr-[3px]">
                       <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M14 7H16C18.7614 7 21 9.23858 21 12C21 14.7614 18.7614 17 16 17H14M10 7H8C5.23858 7 3 9.23858 3 12C3 14.7614 5.23858 17 8 17H10M8 12H16" stroke="#3f9f42" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                       </svg>
-                    </span>
+                    </span> */}
                     <a
                       href={
                         combinedResponses[currentIndex]?.website &&
@@ -1723,10 +1732,14 @@ const Output: React.FC<OutputInterface> = ({
                       target="_blank"
                       rel="noopener noreferrer"
                     >
-                      {combinedResponses[currentIndex]?.website || "NA"}
+                      <span className="inline-block relative top-[8px] mr-[3px]">
+                        <svg width="26px" height="26px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M9.83824 18.4467C10.0103 18.7692 10.1826 19.0598 10.3473 19.3173C8.59745 18.9238 7.07906 17.9187 6.02838 16.5383C6.72181 16.1478 7.60995 15.743 8.67766 15.4468C8.98112 16.637 9.40924 17.6423 9.83824 18.4467ZM11.1618 17.7408C10.7891 17.0421 10.4156 16.1695 10.1465 15.1356C10.7258 15.0496 11.3442 15 12.0001 15C12.6559 15 13.2743 15.0496 13.8535 15.1355C13.5844 16.1695 13.2109 17.0421 12.8382 17.7408C12.5394 18.3011 12.2417 18.7484 12 19.0757C11.7583 18.7484 11.4606 18.3011 11.1618 17.7408ZM9.75 12C9.75 12.5841 9.7893 13.1385 9.8586 13.6619C10.5269 13.5594 11.2414 13.5 12.0001 13.5C12.7587 13.5 13.4732 13.5593 14.1414 13.6619C14.2107 13.1384 14.25 12.5841 14.25 12C14.25 11.4159 14.2107 10.8616 14.1414 10.3381C13.4732 10.4406 12.7587 10.5 12.0001 10.5C11.2414 10.5 10.5269 10.4406 9.8586 10.3381C9.7893 10.8615 9.75 11.4159 9.75 12ZM8.38688 10.0288C8.29977 10.6478 8.25 11.3054 8.25 12C8.25 12.6946 8.29977 13.3522 8.38688 13.9712C7.11338 14.3131 6.05882 14.7952 5.24324 15.2591C4.76698 14.2736 4.5 13.168 4.5 12C4.5 10.832 4.76698 9.72644 5.24323 8.74088C6.05872 9.20472 7.1133 9.68686 8.38688 10.0288ZM10.1465 8.86445C10.7258 8.95042 11.3442 9 12.0001 9C12.6559 9 13.2743 8.95043 13.8535 8.86447C13.5844 7.83055 13.2109 6.95793 12.8382 6.2592C12.5394 5.69894 12.2417 5.25156 12 4.92432C11.7583 5.25156 11.4606 5.69894 11.1618 6.25918C10.7891 6.95791 10.4156 7.83053 10.1465 8.86445ZM15.6131 10.0289C15.7002 10.6479 15.75 11.3055 15.75 12C15.75 12.6946 15.7002 13.3521 15.6131 13.9711C16.8866 14.3131 17.9412 14.7952 18.7568 15.2591C19.233 14.2735 19.5 13.1679 19.5 12C19.5 10.8321 19.233 9.72647 18.7568 8.74093C17.9413 9.20477 16.8867 9.6869 15.6131 10.0289ZM17.9716 7.46178C17.2781 7.85231 16.39 8.25705 15.3224 8.55328C15.0189 7.36304 14.5908 6.35769 14.1618 5.55332C13.9897 5.23077 13.8174 4.94025 13.6527 4.6827C15.4026 5.07623 16.921 6.08136 17.9716 7.46178ZM8.67765 8.55325C7.61001 8.25701 6.7219 7.85227 6.02839 7.46173C7.07906 6.08134 8.59745 5.07623 10.3472 4.6827C10.1826 4.94025 10.0103 5.23076 9.83823 5.5533C9.40924 6.35767 8.98112 7.36301 8.67765 8.55325ZM15.3224 15.4467C15.0189 16.637 14.5908 17.6423 14.1618 18.4467C13.9897 18.7692 13.8174 19.0598 13.6527 19.3173C15.4026 18.9238 16.921 17.9186 17.9717 16.5382C17.2782 16.1477 16.3901 15.743 15.3224 15.4467ZM12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21Z" fill="#3f9f42"/>
+                        </svg>
+                      </span>
+                      {/* {combinedResponses[currentIndex]?.website || "NA"} */}
                     </a>
-                    <span style={{ whiteSpace: "pre" }}> </span>|
-                    <span style={{ whiteSpace: "pre" }}> </span>
+                    {/* <span style={{ whiteSpace: "pre" }}> </span>| */}
                     <ReactTooltip anchorSelect="#li-icon-tooltip" place="top">
                       Open this contact in LinkedIn
                     </ReactTooltip>
