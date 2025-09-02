56699808956471b0c1c262bd5349ffa723aa6cdd
Rushikesh <rushikeshg@groupji.co>
2025-09-02 22:03:47 +0530

    tooltip added

--- src/components/feature/Output.tsx
+++ src/components/feature/Output.tsx
@@ -1750,6 +1750,9 @@ const handleSaveSettings = async () => {
                         <path d="M14 7H16C18.7614 7 21 9.23858 21 12C21 14.7614 18.7614 17 16 17H14M10 7H8C5.23858 7 3 9.23858 3 12C3 14.7614 5.23858 17 8 17H10M8 12H16" stroke="#3f9f42" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                       </svg>
                     </span> */}
+                    <ReactTooltip anchorSelect="#website-icon-tooltip" place="top">
+  Open company website
+</ReactTooltip>
                     <a
                       href={
                         combinedResponses[currentIndex]?.website &&
@@ -1761,6 +1764,8 @@ const handleSaveSettings = async () => {
                       }
                       target="_blank"
                       rel="noopener noreferrer"
+                        id="website-icon-tooltip" 
+
                     >
                       <span className="inline-block relative top-[8px] mr-[3px]">
                         <svg width="26px" height="26px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
