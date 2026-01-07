fe40b50641ac63e945154a436e14f9bb43f6de93
Rushikesh <rushikeshg@groupji.co>
2026-01-07 15:01:02 +0530

    Update API base URL to point to production environment

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 const API_BASE_URL: string =
-  process.env.REACT_APP_API_BASE_URL || "https://test.pitchkraft.ai";
+  process.env.REACT_APP_API_BASE_URL || "https://app.pitchkraft.ai";
 // process.env.REACT_APP_API_BASE_URL || "https://localhost:7216";
 
 //
